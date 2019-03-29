9c0e24d0a6813925a872ff0dd81f09cbb3f12f20
Aleksandr Spiridonov <sasha.aos@gmail.com>
2019-03-29 01:23:15 -0400

    removed compiled files

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface IElementPath {
-    tagName: string;
-    index: number;
-    child: IElementPath | null;
-}
-export declare function createElementPath(element: HTMLElement, childPath?: IElementPath | null): IElementPath;
-export declare function navigateToElementByPath(currentElement: HTMLElement, xPath: IElementPath): HTMLElement | null;
